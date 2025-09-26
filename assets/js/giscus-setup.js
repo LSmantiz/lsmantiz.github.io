944e11487834daac3f78de54985b73318da445f3
LSmantiz <73502299+LSmantiz@users.noreply.github.com>
2025-09-26 08:04:51 +0000

    Deploying to gh-pages from @ LSmantiz/lsmantiz.github.io@ef4e028026101d5c5a2f95d14c0c85d422b7b37e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "LSmantiz/website",
+    "data-repo": "LSmantiz/lsmantiz.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
